9f058c506e84b4ba7eeb1dce09f02993933038c8
Kostya Morenov <kostya.morenov@gmail.com>
2018-05-03 03:56:26 +0300

    v.0.3.1. Moved controllers selection from index.php to Router.php

--- Router.php
+++ Router.php
@@ -1,17 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kostya
- * Date: 1/05/18
- * Time: 3:58 PM
- */
-/*
+
 class Router
 {
     function start()
     {
-        var_dump($_SERVER);
+        if (!empty($_GET['id'])){
+            $filename = $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/" . "NewsController.php";
+        }
+
+        if (!empty($_GET['page'])) {
+            $filename = $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/HomeController.php";
+        }
+
+        if(strtolower($_SERVER['REQUEST_URI']) == FOLDER){
+            require_once $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/HomeController.php";
+        }elseif(!empty($filename) && file_exists($filename)){
+            require_once $filename;
+        }else{
+            require_once $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/404.php";
+        }
     }
 
 }
-*/
--- index.php
+++ index.php
@@ -3,26 +3,11 @@ ini_set('display_errors', 1);
 session_start();
 const FOLDER = "/hillel-mvc_project_1/";
 
-require_once "Router.php";
-
-//Router::start();
-
 require_once $_SERVER['DOCUMENT_ROOT'] . FOLDER . "config.php";
 
-if (!empty($_GET['id'])){
-    $filename = $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/" . "NewsController.php";
-}
+require_once "Router.php";
 
-if (!empty($_GET['page'])) {
-    $filename = $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/HomeController.php";
-}
+Router::start();
 
-if(strtolower($_SERVER['REQUEST_URI']) == FOLDER){
-    require_once $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/HomeController.php";
-}elseif(!empty($filename) && file_exists($filename)){
-    require_once $filename;
-}else{
-    require_once $_SERVER['DOCUMENT_ROOT'] . FOLDER . "controllers/404.php";
-}
 ?>
 
